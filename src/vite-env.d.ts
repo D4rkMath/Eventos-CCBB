5b670b1b64827f203fc8ed27596ba051cf16bd6c
D4rkLorD <135694320+D4rkMath@users.noreply.github.com>
2025-08-27 16:23:13 -0500

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+// vite-env.d.ts: Archivo de declaración para TypeScript
+// Extiende los tipos globales para soportar las funcionalidades de Vite
+// Permite el uso de import.meta.env y otros recursos de Vite en el proyecto Científica Flix
